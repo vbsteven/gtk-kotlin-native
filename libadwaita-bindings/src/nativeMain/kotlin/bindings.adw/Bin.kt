3dd4f34b0c1164b7d613ae5ddb73808a674ea0c1
Steven Van Bael <steven@quantus.io>
2022-12-29 13:56:45 +0100

    adwaita: Add typeInfo to the Bin class so it can be subclassed

--- libadwaita-bindings/src/nativeMain/kotlin/bindings.adw/Bin.kt
+++ libadwaita-bindings/src/nativeMain/kotlin/bindings.adw/Bin.kt
@@ -1,15 +1,15 @@
 package bindings.adw
 
+import bindings.gobject.ObjectCompanion
 import bindings.gobject.asTypedPointer
 import bindings.gtk.Widget
 import bindings.gtk.asWidget
+import internal.BuiltinTypeInfo
 import kotlinx.cinterop.CPointer
-import native.adwaita.AdwBin
-import native.adwaita.adw_bin_get_child
-import native.adwaita.adw_bin_new
-import native.adwaita.adw_bin_set_child
+import kotlinx.cinterop.sizeOf
+import native.adwaita.*
 
-class Bin : Widget {
+open class Bin : Widget {
     val adwBinPointer get() = gtkWidgetPointer.asTypedPointer<AdwBin>()
 
     constructor(pointer: CPointer<*>) : super(pointer)
@@ -18,4 +18,14 @@ class Bin : Widget {
     var child: Widget?
         get() = adw_bin_get_child(adwBinPointer)?.asWidget()
         set(value) = adw_bin_set_child(adwBinPointer, value?.gtkWidgetPointer)
+
+    companion object : ObjectCompanion<Bin>(BinTypeInfo)
 }
+
+private val BinTypeInfo = BuiltinTypeInfo(
+    "AdwBin",
+    ADW_TYPE_BIN,
+    sizeOf<AdwBinClass>(),
+    sizeOf<AdwBin>(),
+    ::Bin
+)
