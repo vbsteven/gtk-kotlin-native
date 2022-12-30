0a4794d3cf721175825faf35b4271b28966af994
Steven Van Bael <steven@quantus.io>
2022-12-30 11:13:05 +0100

    gtk: Add properties and methods to Label class

--- gtk-bindings/src/nativeMain/kotlin/bindings/gtk/Label.kt
+++ gtk-bindings/src/nativeMain/kotlin/bindings/gtk/Label.kt
@@ -1,9 +1,11 @@
 package bindings.gtk
 
+import bindings.gio.MenuModel
+import bindings.gio.asMenuModel
 import bindings.gobject.asTypedPointer
-import kotlinx.cinterop.CPointer
-import kotlinx.cinterop.reinterpret
-import kotlinx.cinterop.toKString
+import bindings.gobject.boolean
+import bindings.gobject.gboolean
+import kotlinx.cinterop.*
 import native.gtk.*
 
 class Label(pointer: CPointer<*>) : Widget(pointer) {
@@ -21,15 +23,117 @@ class Label(pointer: CPointer<*>) : Widget(pointer) {
     constructor(text: String? = null, withMnemonic: Boolean = false) : this(newPointer(text, withMnemonic))
 
     var text: String
-        get() = gtk_label_get_text(gPointer.reinterpret())?.toKString() ?: ""
-        set(value) = gtk_label_set_text(gPointer.reinterpret(), value)
+        get() = gtk_label_get_text(gtkLabelPointer)?.toKString() ?: ""
+        set(value) = gtk_label_set_text(gtkLabelPointer, value)
+
+    val currentUri: String?
+        get() = gtk_label_get_current_uri(gtkLabelPointer)?.toKString()
+
+    var ellipsize: PangoEllipsizeMode
+        get() = gtk_label_get_ellipsize(gtkLabelPointer)
+        set(value) = gtk_label_set_ellipsize(gtkLabelPointer, value)
+
+    var extraMenu: MenuModel?
+        get() = gtk_label_get_extra_menu(gtkLabelPointer)?.asMenuModel()
+        set(value) = gtk_label_set_extra_menu(gtkLabelPointer, value?.gMenuModelPointer)
+
+    var justify: GtkJustification
+        get() = gtk_label_get_justify(gtkLabelPointer)
+        set(value) = gtk_label_set_justify(gtkLabelPointer, value)
+
+    var label: String?
+        get() = gtk_label_get_label(gtkLabelPointer)?.toString()
+        set(value) = gtk_label_set_label(gtkLabelPointer, value)
+
+    // TODO layout and layoutOffsets properties with PangoLayout
+
+    var lines: Int
+        get() = gtk_label_get_lines(gtkLabelPointer)
+        set(value) = gtk_label_set_lines(gtkLabelPointer, value)
+
+    var maxWidthChars: Int
+        get() = gtk_label_get_max_width_chars(gtkLabelPointer)
+        set(value) = gtk_label_set_max_width_chars(gtkLabelPointer, value)
+
+    val mnemonicKeyval: Int
+        get() = gtk_label_get_mnemonic_keyval(gtkLabelPointer).toInt()
+
+    var mnemonicWidget: Widget?
+        get() = gtk_label_get_mnemonic_widget(gtkLabelPointer)?.asWidget()
+        set(value) = gtk_label_set_mnemonic_widget(gtkLabelPointer, value?.gtkWidgetPointer)
+
+    var naturalWrapMode: GtkNaturalWrapMode
+        get() = gtk_label_get_natural_wrap_mode(gtkLabelPointer)
+        set(value) = gtk_label_set_natural_wrap_mode(gtkLabelPointer, value)
+
+    var selectable: Boolean
+        get() = gtk_label_get_selectable(gtkLabelPointer).boolean
+        set(value) = gtk_label_set_selectable(gtkLabelPointer, value.gboolean)
+
+    val selectionBounds: IntRange?
+        get() = memScoped {
+            val startPos = alloc<IntVar>()
+            val endPos = alloc<IntVar>()
+
+            val result = gtk_label_get_selection_bounds(gtkLabelPointer, startPos.ptr, endPos.ptr).boolean
+            // TODO verify if the returned endPos value is inclusive or exclusive
+            return if (result) IntRange(startPos.value, endPos.value) else null
+        }
+
+    var singleLineMode: Boolean
+        get() = gtk_label_get_single_line_mode(gtkLabelPointer).boolean
+        set(value) = gtk_label_set_single_line_mode(gtkLabelPointer, value.gboolean)
+
+    // TODO tabs property with PangoTabArray
+
+    var useMarkup: Boolean
+        get() = gtk_label_get_use_markup(gtkLabelPointer).boolean
+        set(value) = gtk_label_set_use_markup(gtkLabelPointer, value.gboolean)
+
+    var useUnderlin: Boolean
+        get() = gtk_label_get_use_underline(gtkLabelPointer).boolean
+        set(value) = gtk_label_set_use_underline(gtkLabelPointer, value.gboolean)
+
+    var widthChars: Int
+        get() = gtk_label_get_width_chars(gtkLabelPointer)
+        set(value) = gtk_label_set_width_chars(gtkLabelPointer, value)
+
+    var wrap: Boolean
+        get() = gtk_label_get_wrap(gtkLabelPointer).boolean
+        set(value) = gtk_label_set_wrap(gtkLabelPointer, value.gboolean)
+
+    var wrapMode: PangoWrapMode
+        get() = gtk_label_get_wrap_mode(gtkLabelPointer)
+        set(value) = gtk_label_set_wrap_mode(gtkLabelPointer, value)
+
+    var xalign: Float
+        get() = gtk_label_get_xalign(gtkLabelPointer)
+        set(value) = gtk_label_set_xalign(gtkLabelPointer, value)
+
+    var yalign: Float
+        get() = gtk_label_get_yalign(gtkLabelPointer)
+        set(value) = gtk_label_set_yalign(gtkLabelPointer, value)
+
+    fun selectRegion(startOffset: Int, endOffset: Int) =
+        gtk_label_select_region(gtkLabelPointer, startOffset, endOffset)
+
+    fun setMarkup(str: String, withMnemonic: Boolean) {
+        if (withMnemonic) gtk_label_set_markup_with_mnemonic(gtkLabelPointer, str)
+        else gtk_label_set_markup(gtkLabelPointer, str)
+    }
+
+    fun setText(str: String, withMnemonic: Boolean) {
+        if (withMnemonic) gtk_label_set_text_with_mnemonic(gtkLabelPointer, str)
+        else gtk_label_set_text(gtkLabelPointer, str)
+    }
+
 
     companion object {
 
         /* helpers for constructor */
         private fun newPointer(text: String?, withMnemonic: Boolean) =
-            if (withMnemonic) newPointerWithLabel(text)
-            else newPointerWithMnemonic(text)
+            if (withMnemonic) newPointerWithMnemonic(text)
+            else newPointerWithLabel(text)
 
         private fun newPointerWithLabel(text: String?) = gtk_label_new(text)!!
         private fun newPointerWithMnemonic(text: String?) = gtk_label_new_with_mnemonic(text)!!
