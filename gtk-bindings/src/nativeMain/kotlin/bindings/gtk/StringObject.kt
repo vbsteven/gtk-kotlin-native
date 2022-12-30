c50c345f05c8cccd937e1e7985d62d26dc8a83b7
Steven Van Bael <steven@quantus.io>
2022-12-30 10:41:53 +0100

    gtk: Add StringObject class

---
+++ gtk-bindings/src/nativeMain/kotlin/bindings/gtk/StringObject.kt
@@ -0,0 +1,20 @@
+package bindings.gtk
+
+import bindings.gobject.Object
+import bindings.gobject.asTypedPointer
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.toKString
+import native.gtk.GtkStringObject
+import native.gtk.gtk_string_object_get_string
+import native.gtk.gtk_string_object_new
+
+class StringObject : Object {
+    val gtkStringObjectPointer get() = gPointer.asTypedPointer<GtkStringObject>()
+
+    constructor(pointer: CPointer<*>) : super(pointer)
+    constructor(str: String) : this(gtk_string_object_new(str)!!)
+
+    val string: String
+        get() = gtk_string_object_get_string(gtkStringObjectPointer)!!.toKString()
+
+}
