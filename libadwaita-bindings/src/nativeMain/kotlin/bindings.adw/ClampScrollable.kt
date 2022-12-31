cbd47fd4c763d68abcdda5277eb5acae35e182ff
Steven Van Bael <steven@quantus.io>
2022-12-31 13:01:09 +0100

    adwaita: Add ClampScrollable class

---
+++ libadwaita-bindings/src/nativeMain/kotlin/bindings.adw/ClampScrollable.kt
@@ -0,0 +1,33 @@
+package bindings.adw
+
+import bindings.gobject.asTypedPointer
+import bindings.gtk.Orientable
+import bindings.gtk.Scrollable
+import bindings.gtk.Widget
+import bindings.gtk.asWidget
+import kotlinx.cinterop.CPointer
+import native.adwaita.*
+import native.gtk.GtkOrientable
+import native.gtk.GtkScrollable
+
+open class ClampScrollable : Widget, Scrollable, Orientable {
+    val adwClampScrollablePointer get() = gtkWidgetPointer.asTypedPointer<AdwClampScrollable>()
+
+    override val gtkScrollablePointer get() = gtkWidgetPointer.asTypedPointer<GtkScrollable>()
+    override val gtkOrientablePointer get() = gtkWidgetPointer.asTypedPointer<GtkOrientable>()
+
+    constructor(pointer: CPointer<*>) : super(pointer)
+    constructor() : this(adw_clamp_scrollable_new()!!)
+
+    var child: Widget?
+        get() = adw_clamp_scrollable_get_child(adwClampScrollablePointer)?.asWidget()
+        set(value) = adw_clamp_scrollable_set_child(adwClampScrollablePointer, value?.gtkWidgetPointer)
+
+    var maximumSize: Int
+        get() = adw_clamp_scrollable_get_maximum_size(adwClampScrollablePointer)
+        set(value) = adw_clamp_scrollable_set_maximum_size(adwClampScrollablePointer, value)
+
+    var tighteningThreshold: Int
+        get() = adw_clamp_scrollable_get_tightening_threshold(adwClampScrollablePointer)
+        set(value) = adw_clamp_scrollable_set_tightening_threshold(adwClampScrollablePointer, value)
+}
